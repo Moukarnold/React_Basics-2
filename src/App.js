b99f852ab528514371fb288e27b2c5b85c7f7b53
Moukarnold <128094305+Moukarnold@users.noreply.github.com>
2023-04-20 12:37:50 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return HelloWorldArticle();
+}
+
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>
+        {" "}
+        <p> Replace me with your Component!</p>
+      </h1>
+    </article>
+  );
+}
